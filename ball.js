87b1e1c7018003905b4ecee46df29302c893bd9e
Andrii Bilytskyi <abilytskyi@gmail.com>
2022-10-14 19:09:27 +0200

    collision between ball and paddle

--- ball.js
+++ ball.js
@@ -42,7 +42,18 @@ export default class Ball {
         }
 
         // check collision with paddle
+        let bottomOfBall = this.position.y + this.radius;
+        let topOfPaddle = this.game.paddle.position.y;
+        let leftSideOfPaddle = this.game.paddle.position.x;
+        let rightSideOfPaddle = this.game.paddle.position.x + this.game.paddle.width;
 
+        if (bottomOfBall >= topOfPaddle &&
+            this.position.x >= leftSideOfPaddle &&
+            this.position.x + this.radius <= rightSideOfPaddle
+            ) {
+            this.speed.y = -this.speed.y;
+            // this.position.y = this.game.paddle.position.y - this.radius;  why this exitst?
+        }
 
     }
 }
